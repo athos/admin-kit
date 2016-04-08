72c0108651cf2a89bcfb6ca730ad3d6a6f7a167e
OHTA Shogo <athos0220@gmail.com>
2016-04-08 09:46:31 +0900

    Add adapter constructor for brevity

--- src/clj/lustered/adapter.clj
+++ src/clj/lustered/adapter.clj
@@ -12,3 +12,26 @@
 
 (defprotocol Delete
   (delete [adapter params]))
+
+(defn make-adapter [{:keys [create read update delete]}]
+  (let [unsupported (fn [op]
+                      (fn [params]
+                        (let [msg (str (name op) " is not supported")]
+                          (throw (UnsupportedOperationException. msg)))))
+        on-create (or create (unsupported :create))
+        on-read (or read (unsupported :read))
+        on-update (or update (unsupported :update))
+        on-delete (or delete (unsupported :delete))]
+    (reify
+      Create
+      (create [this params]
+        (on-create params))
+      Read
+      (read [this params]
+        (on-read params))
+      Update
+      (update [this params]
+        (on-update params))
+      Delete
+      (delete [this params]
+        (on-delete params)))))
