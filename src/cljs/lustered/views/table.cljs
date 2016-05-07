a0d931dc17fcf1e8626e97421f326079314d73dd
OHTA Shogo <athos0220@gmail.com>
2016-05-07 15:37:40 +0900

    Refactor code

--- src/cljs/lustered/views/table.cljs
+++ src/cljs/lustered/views/table.cljs
@@ -22,37 +22,37 @@
        [:i.fa.fa-circle.fa-stack-2x.text-danger]
        [:i.fa.fa-trash.fa-stack-1x.fa-inverse]]]]))
 
-(defn field-header [base-path page-state field-name field-label sortable?]
-  (let [{:keys [page-name order desc?]} page-state]
-    (if sortable?
-      (let [[state icon] (if (= order field-name)
+(defn sortable-field [field-name field-label]
+  (let [base-path (r/subscribe [:base-path])
+        page-state (r/subscribe [:page-state])]
+    (fn [field-name field-label]
+      (let [{:keys [page-name order desc?] :as page-state} @page-state
+            [state icon] (if (= order field-name)
                            [(update page-state :desc? not)
                             (if desc? :fa-sort-desc :fa-sort-asc)]
                            [(-> page-state
                                 (assoc :order field-name :desc? false))
                             :fa-sort])
             state (dissoc state :page-no)
-            uri (utils/page-state->uri base-path state)
+            uri (utils/page-state->uri @base-path state)
             on-click (fn [e]
                        (.preventDefault e)
                        (->> (apply concat state)
                             (apply handlers/move-to page-name)))]
         [:a.sortable {:href uri :on-click on-click}
          [:i.fa {:class icon}]
-         field-label])
-      field-label)))
+         field-label]))))
 
 (defn table-header [fields]
-  (let [base-path (r/subscribe [:base-path])
-        page-state (r/subscribe [:page-state])]
-    (fn [fields]
-      [:thead
-       [:tr
-        (for [{:keys [name label detail? sortable?]} fields
-              :when (not detail?)]
-          ^{:key name}
-          [:th (field-header @base-path @page-state name label sortable?)])
-        [:th]]])))
+  (fn [fields]
+    [:thead
+     [:tr
+      (for [{:keys [name label detail? sortable?]} fields
+            :when (not detail?)]
+        ^{:key name} [:th (if sortable?
+                            [sortable-field name label]
+                            label)])
+      [:th]]]))
 
 (defn table-body [fields]
   (let [items (r/subscribe [:items])]
