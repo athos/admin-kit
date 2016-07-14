7f175193549c9e6e0a13292c14ac91f4bda08c4e
OHTA Shogo <athos0220@gmail.com>
2016-07-14 12:25:41 +0900

    Provide public API for standard startup

--- src/cljs/lib/admin_kit/core.cljs
+++ src/cljs/lib/admin_kit/core.cljs
@@ -5,7 +5,17 @@
             [admin-kit.handlers :as handlers]
             [admin-kit.views.core :as views]))
 
-(defn start []
+(defn init []
   (when-let [[base-path page-state] (utils/uri->page-state (.-location js/window))]
     (handlers/init base-path #(handlers/page-init page-state)))
   (reagent/render [views/app] (.getElementById js/document "app")))
+
+(defn start []
+  (.addEventListener js/window "load" init)
+  (.addEventListener js/window "popstate"
+                     (fn [e]
+                       (when-let [page-state (.-state e)]
+                         (-> page-state
+                             (js->clj :keywordize-keys true)
+                             (update :order keyword)
+                             handlers/page-init)))))
--- src/cljs/standalone/admin_kit/main.cljs
+++ src/cljs/standalone/admin_kit/main.cljs
@@ -1,13 +1,4 @@
 (ns admin-kit.main
-  (:require [admin-kit.core :as admin]
-            [admin-kit.handlers :as handlers]))
+  (:require [admin-kit.core :as admin]))
 
-(.addEventListener js/window "load" admin/start)
-
-(.addEventListener js/window "popstate"
-                   (fn [e]
-                     (when-let [page-state (.-state e)]
-                       (-> page-state
-                           (js->clj :keywordize-keys true)
-                           (update :order keyword)
-                           handlers/page-init))))
+(admin/start)
